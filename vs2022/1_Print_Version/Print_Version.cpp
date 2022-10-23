fa11aeac7abc632fa995a3728d6b48c2ef2be37f
Thomas <thomas.hochstrasser@gmx.net>
2022-10-23 17:36:20 +0200

    updated to 4.6.0

---
+++ vs2022/1_Print_Version/Print_Version.cpp
@@ -0,0 +1,18 @@
+#include "pch.h"
+
+// Include Libraries
+#include<opencv2/opencv.hpp>
+#include<iostream>
+
+int main()
+{   
+    // Print version and exit gracefully.
+
+    std::cout << "OpenCV version : " << CV_VERSION << std::endl;
+    std::cout << "Major version : " << CV_MAJOR_VERSION << std::endl;
+    std::cout << "Minor version : " << CV_MINOR_VERSION << std::endl;
+    std::cout << "Subminor version : " << CV_SUBMINOR_VERSION << std::endl;
+
+    // Exit Program
+    return 0;    
+}
